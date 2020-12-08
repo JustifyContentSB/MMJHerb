b7dffd1477ea5310d4e02b43265a2e34d99780ff
JustifyContentSB <70196752+JustifyContentSB@users.noreply.github.com>
2020-12-08 16:49:18 +0300

    Написал события для меню навигации на телефонах и планшетах, а также модальные окна. Стилизация фиксированной шапки

--- app/js/script.js
+++ app/js/script.js
@@ -1,50 +1,82 @@
-const slider = document.querySelector('.swider-container');
+$(function() {
 
-let mySwiper = new Swiper('.swiper-container', {
- 
-	slidesPreview: 3,
-	spaceBetween: 10,
-	pagination: {
-	    el: '.swiper-pagination',
-	    type: 'bullets',
-  	},
-})
+	// FAQ OPEN
+	let faqOpen = document.querySelectorAll('.faq__open');
+	for(let i = 0; i < faqOpen.length; i++) {
+		faqOpen[i].addEventListener('click', function(e) {
+			e.preventDefault();
+			console.log(this);
+			this.classList.toggle('faq__open--active');
+		});
+	}
 
+	//MODAL
+	let modalCall = $('[data-modal]');
+	modalCall.on('click', function(event) {
+		event.preventDefault();
+		let $this = $(this);
+		let modalId = $this.data('modal');
 
-let faqOpen = document.querySelectorAll('.faq__open');
-for(let i = 0; i < faqOpen.length; i++) {
-	faqOpen[i].addEventListener('click', function(e) {
-		e.preventDefault();
-		console.log(this);
-		this.classList.toggle('faq__open--active');
+		$(modalId).toggleClass('modal-show');
+		$('body').toggleClass('no-scroll');
+
 	});
-}
 
-// document.addEventListener('keydown', function(e) {
-// 	if(e.which === 27) {
-// 		let popupActive = document.querySelector('.popup.open');
-// 		popupClose(popupActive);
-// 	}
-// });
+	$('.modal').on('click', function(event) {
+		let $this = $(this);
+		$(this).removeClass('modal-show');
+		$('body').removeClass('no-scroll');
+	});
 
-// (function () {
-// 	if (!Element.prototype.closest) {
-// 		Element.prototype.closest = function (css) {
-// 			var node = this;
-// 			while (node) {
-// 				if (node.matches(css)) return node;
-// 				else node = node.parentElement;
-// 			}
-// 			return null;
-// 		};
-// 	}
-// })();
+	$('.modal__content').on('click', function(event) {
+		event.stopPropagation();
+	});
 
-// (function () {
-// 	if (!Element.prototype.matches) {
-// 		Element.prototype.matches = Element.prototype.matchesSelector ||
-// 			Element.prototype.webkitMatchesSelector ||
-// 			Element.prototype.mozMatchesSelector ||
-// 			Element.prototype.msMatchesSelector;
-// 	}
-// })();
+	$(document).on('keydown', function(event) {
+		if(event.which === 27) {
+			$('.modal').removeClass('modal-show');
+			$('body').removeClass('no-scroll');
+		}
+	});
+
+	//TOGGLE HEADER
+	$('.header__toggle-one').on('click', function(event) {
+		event.preventDefault();
+
+		$('.header__menu').addClass('header__show');
+		$('.header__menu-bottom').addClass('header__show');
+		$('body').addClass('no-scroll');
+		$('.header__menu-toggle').toggleClass('header__menu-toggle--active');
+	});
+
+	$('.header__menu-toggle').on('click', function(event) {
+		event.preventDefault();
+
+		$('.header__menu-bottom').toggleClass('header__show');
+		$(this).toggleClass('header__menu-toggle--active');
+		$('body').toggleClass('no-scroll');
+	});
+
+	$(window).scroll(function() {
+		let header = $(".header__menu");
+		if(pageYOffset > 100) {
+			header.addClass('header__show');
+		} else  {
+			header.removeClass('header__show');
+		}
+	});
+
+	$(window).scroll(function() {
+		let headerDekstop = $(".header__top-wrapper");
+		let headerInner = $('.header__inner');
+		if(pageYOffset > 200) {
+			headerDekstop.addClass('header__top-fixed');
+			headerInner.addClass('header__inner-top');
+		} else  {
+			headerDekstop.removeClass('header__top-fixed');
+			headerInner.removeClass('header__inner-top');
+		}
+	});
+
+});
+
